22206a473dc01a185a16030bf9bb2b93da5f3d9b
Lukas Tarkowski <tarkowski14@gmail.com>
2021-05-18 22:48:12 +0200

    create user time component

--- react/App.js
+++ react/App.js
@@ -1,8 +1,42 @@
-class  extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+const UserTime = (props) => {
+  return (
+    <>
+      <p>User Time</p>
+      <div className="clock">
+        {`${props.content.hours} : ${props.content.minutes} `}
+      </div>
+    </>
+  );
+};
+
+class Time extends React.Component {
+  state = {
+    userTime: this.getUserTime(),
+  };
+
+  getUserTime() {
+    const time = new Date();
+    return {
+      hours: time.getHours(),
+      minutes: time.getMinutes(),
+    };
+  }
+  setUserTime = () => {
+    const userTime = this.getTime();
+
+    this.setState({ userTime });
+  };
+  componentDidMount() {
+    setInterval(this.setTime, 1000);
+  }
+
+  render() {
+    return (
+      <>
+        <UserTime content={this.state.userTime} />
+      </>
+    );
+  }
 }
- 
-export default ;
+
+ReactDOM.render(<Time />, document.getElementById("root"));
